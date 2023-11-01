6ba55d4a6f52001720e7d40cc81df1d4d3fa3ab1
Vishal-m <vishalmahesh.study@gmail.com>
2023-11-02 01:10:44 +0530

    clerk auth added

--- app/(dashy)/(routes)/page.tsx
+++ app/(dashy)/(routes)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { UserButton } from '@clerk/nextjs';
 
 const Home = () => {
   return (
     <div>
-        This is a protected page
+        <UserButton />
     </div>
   )
 }
